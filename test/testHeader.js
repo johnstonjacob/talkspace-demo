af1f2d9319b7f2337a322a5723ee234962a3945d
Jacob Johnston <jacobjohnston@circleci.com>
2019-05-09 11:21:26 -0500

    change company name

--- test/testHeader.js
+++ test/testHeader.js
@@ -5,7 +5,7 @@ fixture `Testing Header & Hero Section`
 
 test('Check page has welcome message', async t => {
   await t
-      .expect(Selector('.intro-lead-in').innerText).eql('Welcome to Omega Studios');
+      .expect(Selector('.intro-lead-in').innerText).eql('Welcome to Alpha Studios');
 });
 
 test('Check subheading matches marketing template', async t => {
